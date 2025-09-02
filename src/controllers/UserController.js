da6c923641783b9eec079edb052094d94a1421f2
Cesarjes99 <cesarjes1999@gmail.com>
2025-09-02 14:03:49 -0700

    Feat: made signup controller Avoiding username and mail duplication

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,6 @@
+const req = require('express/lib/request');
 const User = require('../models/User');
+const res = require('express/lib/response');
 
 const ctr = {};
 
@@ -17,5 +19,28 @@ ctr.getAllUsers = () => async (req, res, next) => {
     res.status(500).json({ error: error.message });
   }
 };
+
+ctr.Signup = () => async (req, res) => {
+  try {
+    const duplicatedUsername = await User.query().where('username', req.body.username)
+    const duplicatedEmail = await User.query().where('email', req.body.email)
+
+    if(duplicatedUsername.length > 0){
+      throw new Error('Username already exists')
+    } else if (duplicatedEmail.length > 0){
+      throw new Error ('Account with this email already exists')
+    }
+
+    const users = await User.query().insert({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+    const {username, email, id} = users
+    res.json({username, email, id});
+  } catch (error){
+    res.status(500).json({ error: error.message })
+  }
+}
 
 module.exports = ctr;
