98acdf19046181c9c38f91ee679341e4aaab730e
Cesarjes99 <cesarjes1999@gmail.com>
2025-09-03 13:57:23 -0700

    Made items table with a migration

---
+++ seeds/shop_items_table.js
@@ -0,0 +1,19 @@
+
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('items').del()
+  await knex('items').insert([
+    {name: 'potion',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/potion.png'},
+
+    {name: 'hyper potion', 
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/hyper-potion.png'},
+
+    {name: 'max potion', 
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/max-potion.png'},
+
+    {name: 'restore all',
+    image: 'https://static.wikia.nocookie.net/pokemongo_es_gamepedia/images/a/a8/Potions.png/revision/latest?cb=20180912095707'
+    }
+  ]);
+};
