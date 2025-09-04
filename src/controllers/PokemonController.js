97b7827c5443d7dd02ec346c73be8828e43b5771
Cesarjes99 <cesarjes1999@gmail.com>
2025-09-04 16:39:21 -0700

    Created my pokemon seed to test the sold pokemon condtional on the controller

--- src/controllers/PokemonController.js
+++ src/controllers/PokemonController.js
@@ -13,8 +13,9 @@ ctr.Catch = () => async (req, res) => {
 
   const userExists = await User.query().where('id', user_id)
   const pokemonExists = await Pokemon.query().where('id', pokemon_id)
-  const alreadyCaught = await MyPokemon.query().where({user_id, pokemon_id})
+  const alreadyCaught = await MyPokemon.query().where({user_id, pokemon_id, b_active:true})
   const teamSize = await MyPokemon.query().where('user_id', user_id)
+  const soldPokemon = await MyPokemon.query().where({user_id, pokemon_id, b_active:false})
 
   if(userExists.length === 0) {
     throw new Error('User does not exist')
@@ -24,12 +25,18 @@ ctr.Catch = () => async (req, res) => {
     throw new Error('Pokemon does not exist')
   }
 
-  if(alreadyCaught.length > 0){
-    throw new Error('You already have that pokemon')
-  }
 
   if (teamSize.length >=5){
     throw new Error ('Your team is already full')
+  }
+
+  if (soldPokemon.length > 0){
+      await MyPokemon.query().findById(soldPokemon[0].id).patch({ b_active: true });
+  return res.status(200).json({ message: 'Successfully reactivated pokemon' });
+  }
+
+  if(alreadyCaught.length > 0){
+    throw new Error('You already have that pokemon')
   }
 
   await MyPokemon.query().insert({
