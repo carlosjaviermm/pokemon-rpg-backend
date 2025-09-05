986d7255c2e084851fa0daacda9f4d989a598ba2
Cesarjes99 <cesarjes1999@gmail.com>
2025-09-05 12:19:58 -0700

    Made shop model and import it to the shop controller

---
+++ src/controllers/ShopController.js
@@ -0,0 +1,9 @@
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const User = require('../models/User');
+const Pokemon = require('../models/Pokemon')
+const MyPokemon = require('../models/MyPokemon')
+const Shop = require('../models/Shop')
+
+
+const ctr = {};
